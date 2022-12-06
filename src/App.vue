8478ac04fe53ca3ce96a1661177263b2be3d990f
Hieu Trong Nguyen <ngtronghieudng@gmail.com>
2022-12-06 23:25:51 +0700

    create code sample

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { useVueFlow, VueFlow } from '@vue-flow/core'
+import { Controls } from '@vue-flow/additional-components'
+import { initialElements } from '@/components/inital-elements'
+
+const elements: any = ref(initialElements)
+
+const { nodesDraggable } = useVueFlow()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <label class="label" for="draggable">
+      nodesDraggable
+      <input id="draggable" v-model="nodesDraggable" type="checkbox" class="vue-flow__draggable" />
+    </label>
+  </div>
+  <VueFlow v-model="elements">
+    <Controls />
+  </VueFlow>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.vue-flow {
+  min-height: 100vh;
 }
 </style>
